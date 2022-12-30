1f023cc6812ad5e87b0d25847b901a2456b061dc
aspirantmpm <110274028+aspirantmpm@users.noreply.github.com>
2022-12-30 18:57:37 +0000

    Deploying to gh-pages from @ aspirantmpm/goit-js-hw-09@15a047dfcdc720031194cb139ade7cc2e9ffa8fe 🚀

---
+++ 01-color-switcher.0ed1096f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;function r(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){this.setAttribute("disabled",""),e.removeAttribute("disabled"),r(),d=setInterval(r,1e3)})),e.addEventListener("click",(function(){this.setAttribute("disabled",""),t.removeAttribute("disabled"),clearInterval(d)})),e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.0ed1096f.js.map
